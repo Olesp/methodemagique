d7b03a547975ac104788a5caedef326383122905
olespagnon <olespagnon@wallix.com>
2021-01-20 12:05:47 +0100

    add main()

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-#Petit programme calculant les premières et dernières adresses d'un réseau
+# Petit programme calculant les premières et dernières adresses d'un réseau
 
 
 def get_magic_number(list):
@@ -10,21 +10,21 @@ def get_magic_number(list):
 
 
 def input_address():
-    #Permet à l'utilisateur de rentrer une adresse IP
+    # Permet à l'utilisateur de rentrer une adresse IP
     ip_user = input("Entrez l'adresse (ex: 192.168.0.1) :")
     ip = ip_user.split(".")
     return ip
 
 
 def input_mask():
-    #Permet à l'utilisateur de rentrer un masque réseau
+    # Permet à l'utilisateur de rentrer un masque réseau
     netmask_user = input('Entrez un masque réseau (ex: 255.255.255.0) :')
     netmask = netmask_user.split(".")
     return netmask
 
 
 def check_len(list):
-    #Vérifie que la liste compoprte bien 4 éléments
+    # Vérifie que la liste compoprte bien 4 éléments
     if len(list) < 4:
         print("Votre entrée doit contenir 4 octets")
         return False
@@ -33,7 +33,7 @@ def check_len(list):
 
 
 def check_int(list):
-    #Vérifie que la liste ne comporte que des entiers
+    # Vérifie que la liste ne comporte que des entiers
     for i in list:
         try:
             i = int(i)
@@ -52,7 +52,7 @@ def get_multiples(magic_number):
     return multiples
 
 
-def get_addresses(multiples : list, magic_number_index, ip):
+def get_addresses(multiples: list, magic_number_index, ip):
     i = 0
     for i in range(len(multiples)):
         if multiples[i] <= int(ip[magic_number_index]):
@@ -75,30 +75,41 @@ def get_addresses(multiples : list, magic_number_index, ip):
     return first_address, last_address
 
 
-ip = input_address()
-t = False
-while not t:
-    if not check_len(ip):
-        ip = input_address()
-    else:
-        if not check_int(ip):
+def main():
+    """
+    Entry point
+    """
+
+    ip = input_address()
+    t = False
+    while not t:
+        if not check_len(ip):
             ip = input_address()
         else:
-            t = True
+            if not check_int(ip):
+                ip = input_address()
+            else:
+                t = True
 
-netmask = input_mask()
-t = False
-while not t:
-    if not check_len(netmask):
-        netmask = input_mask()
-    else:
-        if not check_int(netmask):
+    netmask = input_mask()
+    t = False
+    while not t:
+        if not check_len(netmask):
             netmask = input_mask()
         else:
-            t = True
+            if not check_int(netmask):
+                netmask = input_mask()
+            else:
+                t = True
 
-magic_number, magic_number_index = get_magic_number(netmask)
-multiples = get_multiples(magic_number)
-first_address, last_address = get_addresses(multiples,magic_number_index,ip)
+    magic_number, magic_number_index = get_magic_number(netmask)
+    multiples = get_multiples(magic_number)
+    first_address, last_address = get_addresses(
+        multiples, magic_number_index, ip)
 
-print("La première adresse disponible est : {}\nLa dernière adresse du réseau est : {}".format(first_address,last_address))
+    print("La première adresse disponible est : {}\nLa dernière adresse du réseau est : {}".format(
+        first_address, last_address))
+
+
+if __name__ == "__main__":
+    main()
